d243bee6719e7dec64bf5c0cd8757d67c64d235a
Sebastian Śledź <1077889+sebas86@users.noreply.github.com>
2024-04-30 17:23:15 +0200

    Add option to ignore currently running kernel for boot-cleanup

--- Python/boot-cleanup.py
+++ Python/boot-cleanup.py
@@ -6,6 +6,7 @@ import os
 import pathlib
 import re
 import shutil
+import subprocess
 
 
 class DefaultArguments:
@@ -13,8 +14,19 @@ class DefaultArguments:
 
 
 def natural_sort_key(s, _nsre=re.compile('([0-9]+)')):
-    return [int(text) if text.isdigit() else text.lower()
-            for text in _nsre.split(s)]
+    return [int(text) if text.isdigit() else text.lower() for text in _nsre.split(s)]
+
+
+def execute_text_command(cmd: list[str], exception_on_error: bool = True) -> str:
+    out = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout, stderr = out.communicate()
+    if exception_on_error and out.returncode != 0:
+        raise Exception(f"Command failed with exit code {out.returncode}: " + ", ".join(cmd))
+    return stdout.decode('utf-8')
+
+
+def get_current_kernel_version() -> str:
+    return execute_text_command(["/usr/bin/uname", "-r"]).splitlines()[0]
 
 
 def get_related_files(base_dir: pathlib.Path, version_postfix: str):
@@ -27,7 +39,7 @@ def log(pretend: bool, verbose: bool, *args):
         print(*args)
 
 
-def cleanup(base_dir: pathlib.Path, keep_versions: int, pretend: bool, verbose: bool):
+def cleanup(base_dir: pathlib.Path, ignore_current_version: bool, keep_versions: int, pretend: bool, verbose: bool):
     assert (keep_versions >= 0)
     versions = []
     for path in glob.glob(str(base_dir.joinpath("vmlinuz-*")), recursive=False):
@@ -39,8 +51,23 @@ def cleanup(base_dir: pathlib.Path, keep_versions: int, pretend: bool, verbose: 
         if not os.path.isdir(removed_dir_path):
             os.mkdir(removed_dir_path)
 
-    versions_to_remove = versions[0: max(0, len(versions) - keep_versions)]
-    versions_to_keep = list(set(versions) - set(versions_to_remove))
+    versions_similar_to_current_kernel = []
+    if not ignore_current_version:
+        current_kernel_version = get_current_kernel_version()
+        versions_similar_to_current_kernel = list(
+            filter(lambda version: str(version).find(current_kernel_version) >= 0, versions))
+        if 0 < len(versions_similar_to_current_kernel):
+            log(pretend, verbose, f"keeping versions looking like currently running kernel ({current_kernel_version}):")
+            for version in sorted(versions_similar_to_current_kernel):
+                log(pretend, verbose, '-', version)
+            versions = list(set(versions) - set(versions_similar_to_current_kernel))
+            versions.sort(key=natural_sort_key)
+
+    versions_to_remove = versions[0: max(0, len(versions) + len(versions_similar_to_current_kernel) - keep_versions)]
+    versions_to_keep = list(set(versions) - set(versions_to_remove) | set(versions_similar_to_current_kernel))
+
+    versions_to_remove.sort(key=natural_sort_key)
+    versions_to_keep.sort(key=natural_sort_key)
 
     log(pretend, verbose, "to remove:" if pretend else "removing:")
     for version in versions_to_remove:
@@ -66,13 +93,16 @@ def cleanup(base_dir: pathlib.Path, keep_versions: int, pretend: bool, verbose: 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--base-dir", default="/boot")
+    parser.add_argument("--ignore-current-version", action="store_true", default=False,
+                        help="Don't try to check and preserve currently running kernel")
+    parser.add_argument("--keep-versions", type=int, default=DefaultArguments.KeepVersions,
+                        help=f"How many versions should be keeped (default: {DefaultArguments.KeepVersions})")
     parser.add_argument("--pretend", action="store_true", default=False)
     parser.add_argument("--verbose", action="store_true", default=False)
-    parser.add_argument("--keep-versions", type=int, default=DefaultArguments.KeepVersions, help=f"How many versions should be keeped (default: {DefaultArguments.KeepVersions})")
     args = parser.parse_args()
 
-    cleanup(pathlib.PosixPath(args.base_dir), keep_versions=args.keep_versions, pretend=args.pretend,
-            verbose=args.verbose)
+    cleanup(pathlib.PosixPath(args.base_dir), ignore_current_version=args.ignore_current_version,
+            keep_versions=args.keep_versions, pretend=args.pretend, verbose=args.verbose)
 
 
 if __name__ == "__main__":
